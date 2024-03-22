f65f9127187f5b74fe41d5f6750bb37f1963cc78
lastovkoi <lastovkoi@gmail.com>
2024-03-22 14:58:38 +0200

    Added filter by genres

--- backend/src/main/java/com/example/demo/repository/FilmRepository.java
+++ backend/src/main/java/com/example/demo/repository/FilmRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface FilmRepository extends JpaRepository<Film, Long> {
 
     @Query("SELECT f FROM Film f WHERE " +
-            "(:genre IS NULL OR LOWER(f.genre) = LOWER(:genre)) AND " +
+            "(:genre IS NULL OR LOWER(f.genre) LIKE CONCAT('%', LOWER(:genre), '%')) AND " +
             "(:year IS NULL OR f.dateYear = :year) AND " +
             "(:language IS NULL OR LOWER(f.language) = LOWER(:language)) AND " +
             "(:rating IS NULL OR f.rating >= :rating)")
